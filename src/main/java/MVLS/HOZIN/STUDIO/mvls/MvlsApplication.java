c96b6c0ee77fecef9722a0a983fdeea172a52a2b
jspark <acejjin77@github.com>
2022-09-21 11:15:54 +0900

    <init> : Spring initializer
    첫 커밋, README 참조바람

---
+++ src/main/java/MVLS/HOZIN/STUDIO/mvls/MvlsApplication.java
@@ -0,0 +1,13 @@
+package MVLS.HOZIN.STUDIO.mvls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvlsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MVLS/HOZIN/STUDIO/mvls/MvlsApplicationTests.java
@@ -0,0 +1,13 @@
+package MVLS.HOZIN.STUDIO.mvls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvlsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
